3de4a3e53f00eb1cbed3e21a1a3fac046f5ebad9
SHANTO <rabbymd55555@gmail.com>
2025-02-06 08:45:48 +0600

    added ternary file

---
+++ shortcut.js
@@ -0,0 +1,21 @@
+const isLeader = true;
+
+/**
+ * short cut for boolean
+ */
+// if(isPassed == true)
+// if(isLeader){
+//     console.log('leader asche! chair charo');
+// }
+// else{
+//     console.log('vitora dukte dibi na');
+// }
+
+const isPassed = false;
+// if(isPassed == false)
+if(!isPassed ){
+    console.log('toke biya diya dimo');
+}
+else{
+    console.log('tumi amader noyon er moni');
+}
