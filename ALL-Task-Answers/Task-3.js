3b88fea7c50acec8eacd10675c48c1c2194b3854
SHANTO <rabbymd55555@gmail.com>
2025-02-06 16:20:34 +0600

    added task-4 file

--- ALL-Task-Answers/Task-3.js
+++ ALL-Task-Answers/Task-3.js
@@ -32,3 +32,29 @@ else if(score<=69 && score>=60){
 else{
     console.log('you are fail');
 }
+
+
+
+
+
+
+
+// /*another type but same ans>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// const mark = 79;
+
+// if(mark>80){
+//     console.log("you got A+");
+// }
+// else if (mark >70){
+//     console.log("you got A");
+// }
+// else if (mark > 60){
+//     console.log("you got B");
+// }
+// else if (mark > 50){
+//     console.log("you got C");
+// }
+// else{
+//     console.log("you are fail!");
+// }
--- ALL-Task-Answers/Task-4.js
+++ ALL-Task-Answers/Task-4.js
@@ -10,3 +10,24 @@ if you get less than 80 go to home and sleep and act sad
 Note: 
 use nested if-else-if-else
 */
+
+const myScore = 95;
+const friendScore = 70;
+if (myScore > 80) {
+  console.log("Inside my find");
+  if (friendScore > 80) {
+    console.log("go for a lunch");
+  }
+  else if (friendScore >= 60) {
+    console.log("good luck next time.");
+  }
+  else if (friendScore >= 40) {
+    console.log("keep your friend's message unseen.");
+  }
+  else  {
+    console.log("block your friend");
+  }
+  
+} else {
+  console.log("go to home and sleep and act sad");
+}
