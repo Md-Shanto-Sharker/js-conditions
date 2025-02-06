95de79075bd9acdc783c9bae6dc24dbe2dc55ba6
SHANTO <rabbymd55555@gmail.com>
2025-02-06 08:31:01 +0600

    added ternary file

---
+++ ternary.js
@@ -0,0 +1,28 @@
+/**** 
+ * TERNARY --->
+ */
+const age = 12;
+
+// if(age >= 18){
+//     console.log('you can vote');
+// }
+// else{
+//     console.log('Ghumai thako');
+// }
+
+/**
+ * Use the ternary oparator..
+ */
+
+age>=18? console.log("you can vote") : console.log('Ghumai thako');;
+
+
+
+
+
+
+
+/**
+ * Optional : semi-advanced ternary
+ */
+
