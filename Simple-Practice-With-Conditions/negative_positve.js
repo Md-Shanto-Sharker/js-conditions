08093fe66cf856f0f98e25f9689f2d7fa51e6296
SHANTO <rabbymd55555@gmail.com>
2025-02-08 00:32:42 +0600

    added leap year problem file

--- Simple-Practice-With-Conditions/negative_positve.js
+++ Simple-Practice-With-Conditions/negative_positve.js
@@ -1,12 +1,10 @@
 // একটি সংখ্যা দেওয়া হবে, সেটি পজিটিভ, নেগেটিভ, নাকি শূন্য তা বের করো।
 
 const number = -2;
-if(number>0){
-    console.log("This is a positive number");
-}
-else if(number===0){
-    console.log("This is Zero");
-}
-else{
-    console.log("This is a negative number");
+if (number > 0) {
+  console.log("This is a positive number");
+} else if (number === 0) {
+  console.log("This is Zero");
+} else {
+  console.log("This is a negative number");
 }
